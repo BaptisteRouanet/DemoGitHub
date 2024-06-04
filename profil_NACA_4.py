acdc230aec4aeaa42ccdad1629750cefda4cfa09
Baptiste Rouanet <baptiste.rnet@gmail.com>
2024-06-04 15:38:27 -0400

    Correction d'utilisation de linspace plutot que arange dans la création des tables. Modification des paramètres du graphique pour réponde aux exigences de l'exercice.

--- profil_NACA_4.py
+++ profil_NACA_4.py
@@ -18,66 +18,95 @@ import matplotlib.pyplot as plt
 
 
 # Tracer le profil NACA à partir des coordonnées (x,y).
-def tracer_profil(x_up, x_down, y_up, y_down, epaisseur_max):
-    print('on rentre dans tracer profil')
+def tracer_profil(x_up, x_down, y_up, y_down, epaisseur_max, type_calcul, i_max_epaisseur):
+
     # quelques parametre pour le graphique
     plt.rcParams['font.size'] = 14
     plt.rcParams['figure.autolayout'] = True
     plt.rcParams['figure.dpi'] = 100
+    # Points de l'epaisseur max
+    x_max = [x_up[i_max_epaisseur], x_down[i_max_epaisseur]]
+    y_max = [y_up[i_max_epaisseur], y_down[i_max_epaisseur]]
 
     # données à tracer
-    plt.plot(x_up, y_up, label="Extrados")
-    plt.plot(x_down, y_down, label="Intrados")
-    plt.title(f'Profil NACA00{epaisseur_max}')
-    plt.savefig(f'Profil NACA00{epaisseur_max}.pdf')
-    plt.savefig(f'Profil NACA00{epaisseur_max}.png')
+    # Epaisseur max
+    plt.plot(x_max, y_max, '--r', linewidth=2)
+    plt.text(x_max[0]*1.1, 0+0.05*y_max[0], "Epaisseur maximum du profil", fontsize='small', fontstyle='italic', color='r')
+    # Intra et Extrados
+    plt.plot(x_up, y_up, label="Extrados", linewidth=3)
+    plt.plot(x_down, y_down, label="Intrados", linewidth=3)
+
+    # Titres, cadrillage et legend
+    plt.title(f'Profil NACA00{epaisseur_max} - {type_calcul}')
+    plt.savefig(f'Profil_NACA00{epaisseur_max}-{type_calcul}.pdf')
+    plt.savefig(f'Profil_NACA00{epaisseur_max}-{type_calcul}.png')
+    plt.legend()
+    plt.grid(color='k', linestyle='-', linewidth=1)
+    plt.xlabel("Corde")
+    plt.ylabel("Epaisseur")
+
+    # Affichage
     plt.show()
 
 
 # Créer un tableau des coordonnées a partir des valeurs demandées à l'utilisateur.
 def construire_coordonnees(derniers_digits, corde, nbr_points, type_distrib):
-    print('on rentre dans construire coordonnees')
     t_max = derniers_digits/100
     y_t = np.zeros(nbr_points)
+
+    # Calcul lineaire
     if type_distrib == 1:
-        x_c = np.arange(0, 1, 1 / (nbr_points - 1), dtype=float)
+        type_calcul = "Lineaire"
+        x_c = np.linspace(0, 1, nbr_points - 1, dtype=float)
         x_c = np.append(x_c, 1.0)
         for i in range(nbr_points):
             y_t[i] = 5 * t_max * (0.2969 * np.sqrt(x_c[i]) - 0.1260 * x_c[i] - 0.3516 * x_c[i] ** 2 + 0.2843 * x_c[i] ** 3 - 0.1036 * x_c[i] ** 4)
+
+    # Calcul non-uniforme
     else:
-        theta_points = np.arange(0, np.pi, 1 / (nbr_points - 1), dtype=float)
+        type_calcul = "Non-uniforme"
+        theta_points = np.linspace(0, np.pi, nbr_points - 1, dtype=float)
         theta_points = np.append(theta_points, np.pi)
-        print(theta_points)
         x_c = np.zeros_like(theta_points)
         for i in range(nbr_points):
             x_c[i] = 0.5 * (1 - np.cos(theta_points[i]))
             y_t[i] = 5 * t_max * (0.2969 * np.sqrt(x_c[i]) - 0.1260 * x_c[i] - 0.3516 * x_c[i] ** 2 + 0.2843 * x_c[i] ** 3 - 0.1036 * x_c[i] ** 4)
+
+    # tables de coordonnées des points des courbes à tracer
     x_up = x_c * corde
     x_down = x_c * corde
     y_up = y_t * corde
     y_down = -y_t * corde
-    return x_up, x_down, y_up, y_down
+
+    return x_up, x_down, y_up, y_down, type_calcul
 
 
 # Trouver l'epaisseur maximum du profil à partir des coordonnées construites.
 def trouver_epaisseur_max(x_up, y_up):
-    print('on rentre dans epaisseur max')
-    return float(x_up[np.argmax(y_up)])
+    print("L'épaisseur maximum du profil est :")
+    return float(x_up[np.argmax(y_up)]), np.argmax(y_up)
 
 
 # Lance les fonctions avec les entrées utilisateur pour obtenir les données de sortie demandées
 def lancer_code():
+    # Lancement du code
     print("Ce code permet de tracer un profil NACA 4 digit symétrique.\n"
-          "Pour cela il faudra fournir les deux dernier digits : NACA00XX\n"
+          "Pour cela il faudra fournir les deux derniers digits : NACA00XX\n"
           "Ils représentent le pourcentage de corde à laquelle se trouve l’épaisseur maximale du profil\n"
           "Il sera également demandé la corde du profil (la distance entre les bords d'attaque et de fuite.\n")
+    # Input des entrées (corde, deux digits, nbr de point et type de calcul)
     corde = float(input("Quel est la corde 'c' (Ex: c = 12.4) ?    c = "))
     derniers_digits = int(input("Quels sont les deux derniers digits 'NACA00XX' (Ex: NACA0045 --> t = 45) ?    t = "))
     nbr_points = int(input("Combien de points souhaitez vous pour la précision ?   n = "))
     type_distrib = int(input("Quel type de distribution souhaitez vous (linéaire = 1 / non-uniforme = 2)?   1/2 = "))
-    x_up, x_down, y_up, y_down = construire_coordonnees(derniers_digits, corde, nbr_points, type_distrib)
-    print(trouver_epaisseur_max(x_up, y_up))
-    tracer_profil(x_up, x_down, y_up, y_down, derniers_digits)
+    # On récupère les données calculées à l'aide des données en input et la fonction construire_coordonnees
+    x_up, x_down, y_up, y_down, type_calcul = construire_coordonnees(derniers_digits, corde, nbr_points, type_distrib)
+    # On récupère et affiche l'épaisseur max à l'aide de trouver_epaisseur_max
+    x_max_epaisseur, i_max_epaisseur = trouver_epaisseur_max(x_up, y_up)
+    print(f"%.2f" % x_max_epaisseur)
+    # On trace le profil
+    tracer_profil(x_up, x_down, y_up, y_down, derniers_digits, type_calcul, i_max_epaisseur)
 
 
+# Appel de la fonction pour lancer le code
 lancer_code()
